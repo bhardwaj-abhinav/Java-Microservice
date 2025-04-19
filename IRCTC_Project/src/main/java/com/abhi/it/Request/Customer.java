6f908d964cdda42bd1d6374c757c1c918c5536c3
Abhinav <abhinavbhardwaj898@gmail.com>
2025-04-19 11:19:16 +0530

    IRCTC project

---
+++ IRCTC_Project/src/main/java/com/abhi/it/Request/Customer.java
@@ -0,0 +1,13 @@
+package com.abhi.it.Request;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+	private String name;
+	private String from;
+	private String to;
+	private String doj;
+	private String trainNumber;
+	
+}
